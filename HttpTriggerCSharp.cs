d7295661626d3c51a1f91b7d4fc0c1d7be1966e5
Hidenori Ikeda <hidikeda7630@gmail.com>
2019-11-17 12:12:35 +0900

    11/17

--- HttpTriggerCSharp.cs
+++ HttpTriggerCSharp.cs
@@ -26,7 +26,7 @@ namespace Company.Function
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name},good morning!")
+                ? (ActionResult)new OkObjectResult($"Hello, {name},good evening!")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
